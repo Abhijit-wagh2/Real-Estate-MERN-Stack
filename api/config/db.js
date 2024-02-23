6d19813f1428fae73dbf31ae1bb410ab0036fd45
abhijit2712 <74667882+abhijit2712@users.noreply.github.com>
2024-02-23 17:17:27 +0530

    Database Connection

---
+++ api/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async() =>{
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log("connected to database")
+    }catch(error){
+        console.log(error);
+    }
+}
+
+
+export default connectDB;
--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
 const app = express();
+import connectDB from "./config/db.js";
+import dotenv from 'dotenv';
 
+//configure env
+dotenv.config();
+
+connectDB();
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!');
